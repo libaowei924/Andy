1d6ed65312680a484723ec5e7fc89c7e38a42445
libaowei924 <libaowei924@gmail.com>
2018-11-19 12:04:27 +0000

    应用设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Andy - 优雅的和代码沟通</title>
+        <title>{{ config('app.name') }} - 优雅的和代码沟通</title>
 
         <link rel="stylesheet" type="text/css" href="/css/app.css">
 
