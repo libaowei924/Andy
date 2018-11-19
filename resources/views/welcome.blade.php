b4ba1bb5c846ba468ef27282056d07baffe233ad
libaowei924 <libaowei924@gmail.com>
2018-11-19 10:59:56 +0000

    semantic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
 
         <!-- Styles -->
         <style>
@@ -94,5 +94,13 @@
                 </div>
             </div>
         </div>
+
+        <div class="ui message">
+          <i class="close icon"></i>
+          <div class="header">欢迎回来！ </div>
+          <p>如果你觉得厌烦，这个特别的通知可以关闭。</p>
+        </div>
+
+        <script type="text/javascript" src="/js/app.js"></script>
     </body>
 </html>
