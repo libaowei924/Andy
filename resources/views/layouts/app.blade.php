f13ff043956dc4241816f43ac88b4fcf36c1d533
libaowei924 <libaowei924@gmail.com>
2018-11-19 12:29:58 +0000

    修改地址栏颜色

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,12 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="theme-color" content="#27AE60"/>
         <meta name="Description" content="Andy,你可以在这里阅读喜欢的文章">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name') }} - 优雅的和代码沟通</title>
-
+        <link rel="manifest" href="/manifest.json">
         <link rel="stylesheet" type="text/css" href="/css/app.css">
 
     </head>
