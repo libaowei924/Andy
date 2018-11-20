b3d8ff88a9abd6c92731732c8fbf35b16756b359
libaowei924 <libaowei924@gmail.com>
2018-11-20 13:45:39 +0000

    新增首页文章显示样式

--- resources/views/root/index.blade.php
+++ resources/views/root/index.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
   <div class="pusher">
-    <div class="ui basic segment">
-      <h3 class="ui header">首页</h3>
+    <div class="ui divided items">
+        <div class="item">
+            <div class="image">
+              <img src="/images/wireframe/image.png">
+            </div>
+            <div class="content">
+              <a class="header">12 Years a Slave</a>
+              <div class="meta">
+                <span class="cinema">Union Square 14</span>
+              </div>
+              <div class="description">
+                <p></p>
+              </div>
+              <div class="extra">
+                <div class="ui label">IMAX</div>
+                <div class="ui label"><i class="globe icon"></i> Additional Languages</div>
+              </div>
+            </div>
+        </div>
     </div>
   </div>
 @stop
