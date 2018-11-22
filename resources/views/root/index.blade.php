9707d593b0f4b72795367b3ae6a40115cca7ea53
libaowei924 <libaowei924@gmail.com>
2018-11-22 03:25:46 +0000

    hello word

--- resources/views/root/index.blade.php
+++ resources/views/root/index.blade.php
@@ -8,16 +8,15 @@
               <img src="https://ss.csdn.net/p?http://mmbiz.qpic.cn/mmbiz_png/1hReHaqafafs434nZR8TOWXOqC3QKibXsd6Y1hEsXWwOEe67xhy4clBuVlEdibJ5CQGTn4J4DdJC6gjL8kDPT5eA/640?wx_fmt=png&wxfrom=5&wx_lazy=1">
             </div>
             <div class="content">
-              <a class="header">12 Years a Slave</a>
+              <a class="header">Hello - Word</a>
               <div class="meta">
-                <span class="cinema">Union Square 14</span>
+                <span class="cinema">世界我来了～</span>
               </div>
               <div class="description">
                 <p></p>
               </div>
               <div class="extra">
-                <div class="ui label">IMAX</div>
-                <div class="ui label"><i class="globe icon"></i> Additional Languages</div>
+                <div class="ui label">其他</div>
               </div>
             </div>
         </div>
