3774f77b0a8f50d05dcfcb22d75f1a5f71ce5c34
libaowei924 <libaowei924@gmail.com>
2018-11-22 02:15:59 +0000

    新增昵称

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,8 +3,10 @@
     <div class="ui two column centered grid" id="tx">
        <div class="column centered">
          <img class="ui tiny circular image tx" src="/images/tx.png">
+         <b class="nc">Andy</b>
        </div>
     </div>
+    <div class="ui horizontal divider">OR</div>
     <a class="item" href="{{ route('home') }}"><i class="home icon"></i> Home </a>
     <a class="item"><i class="laravel icon"></i> Laravel </a>
     <a class="item"><i class="linux icon"></i> Linux </a>
