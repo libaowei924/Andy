e84093f5eb1118ffb59a911d00bca2e9e0ded921
libaowei924 <libaowei924@gmail.com>
2018-11-22 12:00:24 +0000

    首页数据读取

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends Controller
     public function index()
     {
         $articles = Article::Limit(10)->orderBy('top','desc')->get();
+
         return view('root.index',compact('articles'));
     }
 }
