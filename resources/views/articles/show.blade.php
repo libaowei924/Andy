c0863530e7145f73b32d0eda9c142095673082f0
libaowei924 <libaowei924@gmail.com>
2018-11-26 02:44:05 +0000

    文章查看页面样式调整

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
 <div class="pusher">
-  <div class="ui green segment">
+  <div class="ui green segment" id="article">
     <h1>{{ $article->name }}</h1>
+    <div class="ui labeled button like" tabindex="0">
+        <div class="ui button"><i class="heart icon"></i> 喜欢 </div>
+        <a class="ui basic left pointing label">
+            {{ $article->like }}
+        </a>
+    </div>
     <div class="ui divider"></div>
     @php
         $parsedown = new Parsedown;
